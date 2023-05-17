c17fcb33268657dad33ce9b070a2c6135c6ec0a7
RETOVSKEJ <RETOVSKEJ@wp.pl>
2023-05-17 21:12:56 +0200

    deploy bug fixes

--- services/queries.js
+++ services/queries.js
@@ -1,5 +1,5 @@
 const Post = require('../models/Post')
-const User = require('../models/User')
+const User = require('../models/user')
 
 async function getAllPosts(query) {
 	const regex = new RegExp(query)
